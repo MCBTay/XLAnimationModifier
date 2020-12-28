c7f2728603ece8566877f6870903ac694d861e6b
Bryan Taylor <mcbtay@gmail.com>
2020-12-28 16:22:27 -0500

    adding a patch into the powerslide playerstate

---
+++ XLAnimationModifier/Patches/PlayerState_PowerslidePatch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using HarmonyLib;
+
+namespace XLAnimationModifier.Patches
+{
+	public class PlayerState_PowerslidePatch
+	{
+		[HarmonyPatch(typeof(PlayerState_Powerslide), "ListenForRevert")]
+		static class ListenForRevertPatch
+		{
+			static void Postfix(ref bool ____revertTriggered, ref bool ____revertAnimTriggered)
+			{
+				if (!Main.Enabled || !Settings.Instance.DisableRevertAnimation) return;
+
+				if (____revertTriggered && ____revertAnimTriggered)
+				{
+					Debug.Log("XLAnimationModifier: Revert triggered and animation triggered.  Setting to false.");
+					____revertAnimTriggered = false;
+				}
+			}
+		}
+	}
+}
