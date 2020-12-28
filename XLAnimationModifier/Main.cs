74fc8a11e39e9e8f4508a6c0209533e7f4aa38d0
Bryan Taylor <mcbtay@gmail.com>
2020-12-28 16:05:32 -0500

    implementing main to create the user interface object as well as settings

--- XLAnimationModifier/Main.cs
+++ XLAnimationModifier/Main.cs
@@ -1,12 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using UnityModManagerNet;
 
 namespace XLAnimationModifier
 {
-	class Main
+#if DEBUG
+	[EnableReloading]
+#endif
+	static class Main
 	{
+		public static bool Enabled;
+		private static Harmony Harmony;
+		private static GameObject UserInterfaceGameObject;
+
+		static bool Load(UnityModManager.ModEntry modEntry)
+		{
+			Settings.Instance = UnityModManager.ModSettings.Load<Settings>(modEntry);
+			Settings.ModEntry = modEntry;
+
+			UserInterfaceGameObject = new GameObject();
+			UserInterfaceGameObject.AddComponent<UserInterface>();
+			Object.DontDestroyOnLoad(UserInterfaceGameObject);
+
+			modEntry.OnToggle = OnToggle;
+#if DEBUG
+			modEntry.OnUnload = Unload;
+#endif
+
+			return true;
+		}
+
+		private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+		{
+			if (Enabled == value) return true;
+			Enabled = value;
+
+			if (Enabled)
+			{
+				Harmony = new Harmony(modEntry.Info.Id);
+				Harmony.PatchAll(Assembly.GetExecutingAssembly());
+			}
+			else
+			{
+				Object.DestroyImmediate(UserInterfaceGameObject);
+				Harmony.UnpatchAll(Harmony.Id);
+			}
+
+			return true;
+		}
+
+#if DEBUG
+		static bool Unload(UnityModManager.ModEntry modEntry)
+		{
+			Object.DestroyImmediate(UserInterfaceGameObject);
+
+			Harmony?.UnpatchAll();
+			return true;
+		}
+#endif
 	}
 }
